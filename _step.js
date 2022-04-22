4f9d0e4696a285797bd314c8ed331aeea53ea91e
mohiuddin2721 <mohiuddinpatwary27@gmail.com>
2022-04-23 02:32:19 +0600

    steps update

---
+++ _step.js
@@ -0,0 +1,33 @@
+/**
+ * ----------------------------
+ * basic server setup
+ * -----------------------------
+ * 1. create a folder (manually or mkdir)
+ * 2. open command line to that folder
+ * 3. npm init
+ * 4. npm install express 
+ * 5. create index.js
+ * ---------------------------
+ * For express/node server
+ * ---------------------------
+ * 1. require express 
+ * 2. create app variable
+ * 3. declare port
+ * 4. set app.get('/)
+ * 5. listen to port
+ * 6. node index.js
+ * 7. check your browser for that port
+ * ------------------------------
+ * get
+ * ------------------------------
+ * 1. npm install cors (cors middleware)
+ * 2. const core = require('cors'); (type the code on index.js server)
+ * 2. app.use(cors()); (type the code on index.js server)
+ * 
+ * -------------------------------
+ * post 
+ * --------------------------------
+ * 1. google: fetch post data
+ * 2. app.use(express.json()); (type the code on index.js server)
+ * 
+ */
